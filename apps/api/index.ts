ea4a28715378f778d4af45ee2ef4e2f61a932637
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-03-16 23:17:27 +0530

    'modified'

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,15 +1,15 @@
 import Elysia from "elysia";
 import cors from "@elysiajs/cors";
 import config from "./config/config";
+import user from "./routes/user";
 
-const app = new Elysia({prefix: '/api/v1'})
+new Elysia({prefix: '/api/v1'})
 .use(cors({origin: ['localhost:3000']}))
+.use(user)
 
 .get('/health', ({status}) => {
     return status(200, {message: "Server is healthy"})
 })
-
-.post()
 
 .listen(config.port, () => {
     console.log(`server listening on port ${config.port}`);
