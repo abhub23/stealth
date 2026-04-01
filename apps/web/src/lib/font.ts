43d63b8bbb36f4179be2303fd417398f8764ffe5
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-04-02 04:54:41 +0530

    new font added

--- apps/web/src/lib/font.ts
+++ apps/web/src/lib/font.ts
@@ -1,7 +1,15 @@
-import { Lexend } from "next/font/google";
+import { Lexend, Literata } from "next/font/google";
 
 const lexend_font = Lexend({
   weight: "300",
 });
 
+export const literataFont = Literata({
+  weight: '500',
+  subsets: ["latin"],
+  style: "italic",
+  display: "swap",
+});
+
 export const lexend = lexend_font.className;
+export const literata = literataFont.className;
