3d7ad9d45507aad7068bca58bb49680a7d2201dd
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-04-12 20:15:25 +0530

    added auth handler to the main file

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,14 +1,12 @@
 import Elysia from "elysia";
 import cors from "@elysiajs/cors";
 import config from "./config/config";
+import { auth } from "./auth";
 
-new Elysia({ prefix: "/api/v1" })
-  .use(cors({ origin: ["localhost:3000"] }))
-
-  .get("/health", ({ status }) => {
-    return status(200, { message: "Server is healthy" });
-  })
-
+const app = new Elysia({ prefix: "/api/v1" })
+  .use(cors({ origin: ["localhost:3000"], credentials: true }))
+  .mount(auth.handler)
+  .get("/health", () => ({ message: "Server is healthy" }))
   .listen(config.port, () => {
     console.log(`server listening on port ${config.port}`);
   });
