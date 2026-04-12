24f394b356c8f6f51174894e6489b067cb4ddafa
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-04-12 19:09:31 +0530

    added pino

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,11 +1,9 @@
 import Elysia from "elysia";
 import cors from "@elysiajs/cors";
 import config from "./config/config";
-import user from "./routes/user";
 
 new Elysia({ prefix: "/api/v1" })
   .use(cors({ origin: ["localhost:3000"] }))
-  .use(user)
 
   .get("/health", ({ status }) => {
     return status(200, { message: "Server is healthy" });
--- apps/api/routes/user.ts
+++
@@ -1,8 +0,0 @@
-import Elysia from "elysia";
-
-const user = new Elysia().get("/yoyo", ({ redirect, status }) => {
-  // return redirect('www.google.com')
-  return status(200, "hey there");
-});
-
-export default user;
