903bd5afe035be9ba098ee145b387f88bdf50c2a
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-05-24 23:23:34 +0530

    bento section added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,19 +1,83 @@
-import Navbar from "../src/components/navbar";
-import BentoGrid from "../src/components/bento-grid";
-import CTA from "../src/components/cta";
-import FAQs from "../src/components/faqs";
-import Footer from "../src/components/footer";
+import Navbar from "@/components/navbar";
+import CTA from "@/components/cta";
+import FAQs from "@/components/faqs";
+import Footer from "@/components/footer";
+import { BentoCard } from '@/components/bento-card'
+import { Container } from '@/components/container'
+import { Keyboard } from '@/components/keyboard'
+import { LogoCluster } from '@/components/logo-cluster'
+import { Map } from '@/components/map'
+import { Heading, Subheading } from '@/components/text'
+
+function BentoSection() {
+  return (
+    <Container className="pb-10">
+      <Subheading>Sales</Subheading>
+      <Heading as="h3" className="mt-2 max-w-3xl">
+        Know more about your customers than they do.
+      </Heading>
+
+      <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
+        <BentoCard
+          eyebrow="Insight"
+          title="Get perfect clarity"
+          description="Radiant uses social engineering to build a detailed financial picture of your leads. Know their budget, compensation package, social security number, and more."
+          graphic={
+            <div className="h-80 bg-[url(/screenshots/profile.png)] bg-size-[1000px_560px] bg-position-[left_-109px_top_-112px] bg-no-repeat" />
+          }
+          fade={['bottom']}
+          className="max-lg:rounded-t-4xl lg:col-span-3 lg:rounded-tl-4xl"
+        />
+        <BentoCard
+          eyebrow="Analysis"
+          title="Undercut your competitors"
+          description="With our advanced data mining, you'll know which companies your leads are talking to and exactly how much they're being charged."
+          graphic={
+            <div className="absolute inset-0 bg-[url(/screenshots/competitors.png)] bg-size-[1100px_650px] bg-position-[left_-38px_top_-73px] bg-no-repeat" />
+          }
+          fade={['bottom']}
+          className="lg:col-span-3 lg:rounded-tr-4xl"
+        />
+        <BentoCard
+          eyebrow="Speed"
+          title="Built for power users"
+          description="It's never been faster to cold email your entire contact list using our streamlined keyboard shortcuts."
+          graphic={
+            <div className="flex size-full pt-10 pl-10">
+              <Keyboard highlighted={['LeftCommand', 'LeftShift', 'D']} />
+            </div>
+          }
+          className="lg:col-span-2 lg:rounded-bl-4xl"
+        />
+        <BentoCard
+          eyebrow="Source"
+          title="Get the furthest reach"
+          description="Bypass those inconvenient privacy laws to source leads from the most unexpected places."
+          graphic={<LogoCluster />}
+          className="lg:col-span-2"
+        />
+        <BentoCard
+          eyebrow="Limitless"
+          title="Sell globally"
+          description="Radiant helps you sell in locations currently under international embargo."
+          graphic={<Map />}
+          className="max-lg:rounded-b-4xl lg:col-span-2 lg:rounded-br-4xl"
+        />
+      </div>
+    </Container>
+  )
+}
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="pt-20 min-h-[200vh]">
-        <CTA />
-        <BentoGrid />
+      <CTA />
+      <div className="overflow-hidden">
+        <BentoSection />
       </div>
       <FAQs />
       <Footer />
     </>
-  );
+  )
 }
