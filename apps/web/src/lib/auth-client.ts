45a0d62263bcd61b4d0343f413dc69f4f6e55932
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-07-30 21:42:37 +0530

    client for auth

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,12 +1,13 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000", // The base URL of your auth server
+  baseURL: "http://localhost:4000", // The base URL of your auth server
 });
 
 export async function googleSignIn() {
   await authClient.signIn.social({
     provider: "google",
+    callbackURL: "http://localhost:4000"
   });
 }
 
