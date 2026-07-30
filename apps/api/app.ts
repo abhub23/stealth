e124be9f7cb98e4c3dcb7b88db9f4abbdf8830c6
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-07-30 21:28:51 +0530

    added routes

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -2,13 +2,17 @@ import Elysia from "elysia";
 import cors from "@elysiajs/cors";
 import { auth } from "./src/auth";
 import { authRoutes } from "./src/modules/auth/auth.routes";
+import { healthRoutes } from "./src/modules/health/health.routes";
 import { usersRoutes } from "./src/modules/users/users.routes";
 
 const app = new Elysia({ prefix: "/api/v1" })
-  .use(cors({ origin: ["http://localhost:3000"], credentials: true }))
+  .use(cors({
+     origin: ["http://localhost:3000"],
+     credentials: true,
+     allowedHeaders: ["Content-Type", "Authorization"], }))
   .mount(auth.handler)
+  .use(healthRoutes)
   .use(authRoutes)
-  .use(usersRoutes)
-  .get("/health", () => "server is healthy");
+  .use(usersRoutes);
 
 export default app;
