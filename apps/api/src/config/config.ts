6698ca50abd2d8ea876b372fe06d8c4095238e0d
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-08-01 18:58:53 +0530

    config added

--- apps/api/src/config/config.ts
+++ apps/api/src/config/config.ts
@@ -1,5 +1,13 @@
 const config = {
   port: process.env.PORT || 4000,
+  betterAuth: {
+    secret: process.env.BETTER_AUTH_SECRET!,
+    url: process.env.BETTER_AUTH_URL!,
+  },
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  },
 };
 
 export default config;
