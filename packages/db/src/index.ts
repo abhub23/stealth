da1468668caf82a98ef1e15fd6a57e16015b18f6
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-08-03 02:16:53 +0530

    added pg Pool

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,20 @@
-import "dotenv/config";
+import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as authSchema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, {
-  schema: { ...authSchema },
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+try {
+  await pool.query( "SELECT 1");
+
+  console.log("✅ Database connected successfully!");
+} catch (err) {
+  console.error("❌ Database connection failed:");
+  console.error(err);
+}
+
+export const db = drizzle(pool, {
+  schema: authSchema,
 });
